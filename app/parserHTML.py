9c2baf36f7a819bd17191707f38ccecf5f827bc2
Externo - Deppeler Eric <eric.deppeler@nbch.com.ar>
2024-06-09 12:17:54 -0300

    se agrego pruebas para html

---
+++ app/parserHTML.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import os
+
+@pytest.fixture(scope="module")
+def browser():
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    html_file_path = os.path.join(current_dir, "index.html")
+    driver.get(f"file:///{html_file_path}")
+
+    yield driver
+    driver.quit()
+
+def test_title(browser):
+    assert browser.title == "Página prueba v2"
+
+if __name__ == '__main__':
+    pytest.main()
