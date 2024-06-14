71e75803f0fbe5fb124af6b6fdf463a510406151
Externo - Deppeler Eric <eric.deppeler@nbch.com.ar>
2024-06-14 07:34:28 -0300

    arreglo de prueba

--- app/parserHTML.py
+++ app/parserHTML.py
@@ -18,7 +18,7 @@ def browser():
     driver.quit()
 
 def test_title(browser):
-    assert browser.title == "Página pruebav2"
+    assert browser.title == "Página prueba"
 
 if __name__ == '__main__':
     pytest.main()
